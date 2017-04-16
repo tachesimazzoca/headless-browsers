c015717cb0652efda8ab118a9449df647873ec61
Takeshi Matsuoka <tachesimazzoca@gmail.com>
2017-04-16 22:23:43 +0900

    Add cdp

---
+++ cdp/scripts/runtime.js
@@ -0,0 +1,32 @@
+const commandLineArgs = require('command-line-args');
+const CDP = require('chrome-remote-interface');
+
+const optionDefinitions = [
+  { name: 'host', alias: 'h', type: String, defaultValue: 'localhost' },
+  { name: 'port', alias: 'p', type: Number, defaultValue: 9222 }
+];
+
+const options = commandLineArgs(optionDefinitions);
+
+CDP(options, (client) => {
+  const { Runtime } = client;
+
+  Promise.all([
+    Runtime.enable()
+  ]).then(() => {
+
+    Runtime.evaluate({
+      expression: 'var f = function(a, b) { return a + b; }; f(1, 2);'
+    }).then((result) => {
+      console.log(result.result);
+      client.close();
+    });
+
+  }).catch((err) => {
+    console.error(err);
+    client.close();
+  });
+
+}).on('error', (err) => {
+  console.error(err);
+});
